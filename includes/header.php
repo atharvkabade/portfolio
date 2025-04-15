660ac5c603eb53f81c47df3a206eefd89e9766b5
atharvkabade <kabadeatharv18@gmail.com>
2025-04-15 22:08:08 +0530

    created a new file as project.php which store my project on githup

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,8 @@
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="about.php">About</a></li>
-            <li><a href="services.php">Services</a></li>
+            <!-- <li><a href="services.php">Services</a></li> -->
+            <li><a href="project.php">Project</a></li>
             <li><a href="gallery.php">Gallery</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
---
+++ project.php
@@ -0,0 +1,10 @@
+<?php include 'includes/header.php'; ?>
+<h2>Welcome to My Poject</h2>
+<p>My personal portfolio website serves as a digital showcase of my skills, projects, and experiences. 
+    Designed with a clean and modern layout, the site provides a seamless user experience while reflecting my personal brand and aesthetic.
+    The main goal of this project was to create a platform where I could present my work, share insights about my journey, and make it easier for potential clients or employers to connect with me. 
+    It features sections like an about me, resume, skills, projects, and contact form — all crafted with responsiveness in mind, ensuring a smooth experience across all devices.</p>
+    <a href="https://github.com/atharvkabade/portfolio" target="_blank" style="display: inline-block; padding: 10px 20px; background-color: #24292e; color: white; text-decoration: none; border-radius: 5px; margin-top: 10px;">
+    View on GitHub
+</a>
+<?php include 'includes/footer.php'; ?>
